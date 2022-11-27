03e509988e9335fef87b2a158535f03a166ded83
parkeu <pej6224@naver.com>
2022-11-27 22:35:07 +0900

    사용자두명

--- practice/chat/static/room.js
+++ practice/chat/static/room.js
@@ -80,6 +80,12 @@ function connect() {
                 chatLog.value += data.user + " left the room.\n";
                 onlineUsersSelectorRemove(data.user);
                 break;
+            case "private_message":
+                chatLog.value += "PM from " + data.user + ": " + data.message + "\n";
+                break;
+            case "private_message_delivered":
+                chatLog.value += "PM to " + data.target + ": " + data.message + "\n";
+                break;
             default:
                 console.error("Unknown message type!");
                 break;
@@ -96,3 +102,9 @@ function connect() {
     }
 }
 connect();
+
+onlineUsersSelector.onchange = function() {
+    chatMessageInput.value = "/pm " + onlineUsersSelector.value + " ";
+    onlineUsersSelector.value = null;
+    chatMessageInput.focus();
+};
