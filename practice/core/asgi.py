1ec25844cf44e1e7172c1d44c50e60d29d42cc28
parkeu <pej6224@naver.com>
2022-11-27 22:11:43 +0900

    채팅

---
+++ practice/core/asgi.py
@@ -0,0 +1,19 @@
+
+import os
+
+from channels.auth import AuthMiddlewareStack  # new import
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+import chat.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+  'http': get_asgi_application(),
+  'websocket': AuthMiddlewareStack(  # new
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),  # new
+})
